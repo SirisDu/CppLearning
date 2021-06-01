7df7735860feff535940c176e3380c8d162a2f31
Siris Du <dujr0017@163.com>
2021-06-01 23:05:45 +0800

    testing the template stack class

---
+++ 14.13-14/stacktem/stacktem/stacktem.cpp
@@ -0,0 +1,60 @@
+﻿// stacktem.cpp
+// testing the template stack class
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include "stacktp.h"
+
+int main()
+{
+    Stack<std::string> st;                // create an empty stack
+    char ch{};
+    std::string po{};
+
+    std::cout << "Please enter A to add a purchase order." << std::endl
+        << "P to process a PO, or Q to quit." << std::endl;
+    while (std::cin >> ch && toupper(ch) != 'Q')
+    {
+        while (std::cin.get() != '\n')
+        {
+            continue;
+        }
+        if (!isalpha(ch))
+        {
+            std::cout << '\a';
+            continue;
+        }
+        switch (ch)
+        {
+        case 'A':
+        case 'a':
+            std::cout << "Enter a PO number to add: ";
+            std::cin >> po;
+            if (st.isfull())
+            {
+                std::cout << "stack already full" << std::endl;
+            }
+            else
+            {
+                st.push(po);
+                break;
+            }
+        case 'P':
+        case 'p':
+            if (st.isempty())
+            {
+                std::cout << "stack already empty" << std::endl;
+            }
+            else
+            {
+                st.pop(po);
+                std::cout << "PO #" << po << " popped" << std::endl;
+                break;
+            }
+        }
+        std::cout << "Please enter A to add a purchase order." << std::endl
+            << "P to process a PO, or Q to quit." << std::endl;
+    }
+    std::cout << "Bye." << std::endl;
+}
