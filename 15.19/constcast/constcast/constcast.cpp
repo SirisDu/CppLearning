c6b0a93bfd94917bf350f6126016352ceef1ca9d
Siris Du <dujr0017@163.com>
2021-06-08 23:34:22 +0800

    using const_cast<>

---
+++ 15.19/constcast/constcast/constcast.cpp
@@ -0,0 +1,25 @@
+﻿// constcast.cpp
+// using const_cast<>
+
+#include <iostream>
+
+void change(const int* pt, int n);
+
+int main()
+{
+    int pop1{ 38383 };
+    const int pop2{ 2000 };
+
+    std::cout << "pop1, pop2: " << pop1 << ", " << pop2 << std::endl;
+    change(&pop1, -103);
+    change(&pop2, 103);
+    std::cout << "pop1, pop2: " << pop1 << ", " << pop2 << std::endl;
+}
+
+void change(const int* pt, int n)
+{
+    int* pc;
+
+    pc = const_cast<int*>(pt);
+    *pc += n;
+}
