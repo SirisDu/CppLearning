4dc29a7593d0d3c4413febf51a1ab75acc2f7bef
Siris Du <dujr0017@163.com>
2021-06-14 23:18:08 +0800

    auto_ptr a poor choice

--- 16.6/fowl/fowl/fowl.cpp
+++ 16.6/fowl/fowl/fowl.cpp
@@ -7,14 +7,14 @@
 
 int main()
 {
-    std::auto_ptr<std::string> films[5]{
-        std::auto_ptr<std::string>(new std::string("Fowl Balls")),
-        std::auto_ptr<std::string>(new std::string("Duck Walks")),
-        std::auto_ptr<std::string>(new std::string("Chicken Runs")),
-        std::auto_ptr<std::string>(new std::string("Turkey Errors")),
-        std::auto_ptr<std::string>(new std::string("Goose Eggs"))
+    std::shared_ptr<std::string> films[5]{
+        std::shared_ptr<std::string>(new std::string("Fowl Balls")),
+        std::shared_ptr<std::string>(new std::string("Duck Walks")),
+        std::shared_ptr<std::string>(new std::string("Chicken Runs")),
+        std::shared_ptr<std::string>(new std::string("Turkey Errors")),
+        std::shared_ptr<std::string>(new std::string("Goose Eggs"))
     };
-    std::auto_ptr<std::string> pwin;
+    std::shared_ptr<std::string> pwin;
     pwin = films[2];           // films[2] loses ownership
 
     std::cout << "The nominees for best avian baseball film are" << std::endl;
