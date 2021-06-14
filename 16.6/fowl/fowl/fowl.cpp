25dd35009e36febce5832e709a36e65b52dbe723
Siris Du <dujr0017@163.com>
2021-06-14 23:13:19 +0800

    auto_ptr a poor choice

---
+++ 16.6/fowl/fowl/fowl.cpp
@@ -0,0 +1,26 @@
+﻿// fowl.cpp
+// auto_ptr a poor choice
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+int main()
+{
+    std::auto_ptr<std::string> films[5]{
+        std::auto_ptr<std::string>(new std::string("Fowl Balls")),
+        std::auto_ptr<std::string>(new std::string("Duck Walks")),
+        std::auto_ptr<std::string>(new std::string("Chicken Runs")),
+        std::auto_ptr<std::string>(new std::string("Turkey Errors")),
+        std::auto_ptr<std::string>(new std::string("Goose Eggs"))
+    };
+    std::auto_ptr<std::string> pwin;
+    pwin = films[2];           // films[2] loses ownership
+
+    std::cout << "The nominees for best avian baseball film are" << std::endl;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << *films[i] << std::endl;
+    }
+    std::cout << "The winner is " << *pwin << "!" << std::endl;
+}
