e34707fe0c4d598b71734381851aa0c9a3106c6e
Siris Du <dujr0017@163.com>
2021-06-15 22:49:12 +0800

    introducing the vector template

---
+++ 16.7/vect1/vect1/vect1.cpp
@@ -0,0 +1,31 @@
+﻿// vect1.cpp
+// introducing the vector template
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+const int NUM{ 5 };
+
+int main()
+{
+    std::vector<int> ratings(NUM);
+    std::vector<std::string> titles(NUM);
+
+    std::cout << "You will do exactly as told. You will enter" << std::endl
+        << NUM << " book titles and your ratings (0-10)." << std::endl;
+    for (int i = 0; i < NUM; i++)
+    {
+        std::cout << "Enter title #" << i + 1 << ": ";
+        std::getline(std::cin, titles[i]);
+        std::cout << "Enter your rating (0-10): ";
+        std::cin >> ratings[i];
+        std::cin.get();
+    }
+    std::cout << "Thank you. You entered the following:" << std::endl
+        << "Rating\tBook" << std::endl;
+    for (int i = 0; i < NUM; i++)
+    {
+        std::cout << ratings[i] << "\t" << titles[i] << std::endl;
+    }
+}
